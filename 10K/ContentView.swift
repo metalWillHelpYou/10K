970969712cd7305add0cc037b3ff9a8e4cf2e09d
Daniil Krivonogov <113389092+metalWillHelpYou@users.noreply.github.com>
2025-01-01 21:28:10 +0100

    Initial Commit

---
+++ 10K/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  10K
+//
+//  Created by metalWillHelpYou on 01.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 10K/_0KApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0KApp.swift
+//  10K
+//
+//  Created by metalWillHelpYou on 01.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct _0KApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
